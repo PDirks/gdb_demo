bd5e7dfba67e366c36cb124df19e750e378659b4
Peter Dirks <pld9bc@mail.missouri.edu>
2016-09-14 13:33:30 -0500

    add factorial.c

---
+++ factorial.c
@@ -0,0 +1,23 @@
+/*
+ * gdb demo: factorial.c
+ */
+ 
+ #include <stdio.h>
+ 
+ int main( void ){
+ 
+ 	int num;
+ 	do {
+ 	
+ 	} while ( num < 0 )
+ 	
+ 	int factorial;
+ 	int i;
+ 	for( i = 0; i <= num; i++ ){
+ 		factorial = factorial * i;
+ 	}// end for
+ 
+ 	printf( "%d! = %d\n", num, factorial )
+ 
+ 	return 0;
+ }// end main
